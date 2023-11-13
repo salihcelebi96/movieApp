2718ab3dce79141edd0b7bffb9fa0e02a89378d8
salihcelebi96 <celebisalih277@gmail.com>
2023-11-13 22:21:37 +0300

    21.commit

--- src/components/login.js
+++ src/components/login.js
@@ -24,7 +24,7 @@ const LoginButton = () => {
 
   
   return (
-    <div className='absolute text-2xl   font-semibold border-none p-2  rounded-md  top-3 right-5 hover:bg-gray-300 border bg-white  '>
+    <div className='absolute text-2xl   font-serif border-none p-2  rounded-md  top-3 right-5 hover:bg-gray-300 border bg-white  '>
       {isAuthenticated ? (
         <button className='text-black' onClick={handleLogout}>Logout</button>
       ) : (
