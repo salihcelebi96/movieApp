b61e4e6183ffaf233442fee538aae3d63d41018d
salihcelebi96 <celebisalih277@gmail.com>
2024-02-21 18:04:58 +0300

    10.commit

--- src/components/api.js
+++ src/components/api.js
@@ -14,7 +14,7 @@ function APIPage(props) {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [popularMovies, setPopularMovies] = useState([]);
-   console.log("auth", isAuthenticated)
+    console.log("auth", isAuthenticated)
 
     const handleViewDetail = async (movie) => {
         try {
@@ -43,13 +43,13 @@ function APIPage(props) {
     };
 
     const filterItems = (items, searchTerm) => {
-        if (typeof searchTerm !== 'string') {
-            return [];
+        if (typeof searchTerm !== 'string' || searchTerm.trim() === '') {
+            return items; // Boş arama değeri ise tüm filmleri göster
         }
-
+    
         return items.filter((item) => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
     };
-
+    
     const fetchPopularMovies = async () => {
         try {
             const response = await fetch(`${apiUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`);
@@ -74,6 +74,11 @@ function APIPage(props) {
         fetchPopularMovies();
     }, [apiKey, apiUrl]);
 
+    useEffect(() => {
+        const filteredMovies = filterItems(popularMovies, searchValue);
+        setPopularMovies(filteredMovies);
+    }, [searchValue]);
+
     return (
         <div className='w-full h-full'>
             <div className='flex justify-center pt-3'>
@@ -88,35 +93,40 @@ function APIPage(props) {
                 </form>
             </div>
             <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mt-5 justify-center'>
-                {popularMovies.map((movie) => (
-                    <div className='flex my-10 flex-col justify-center items-center w-[500px] group' key={movie.id}>
-                        <h1 className='w-80 my-4 flex justify-center'>{movie.title}</h1>
-                        <div className='relative'>
-                            <img
-                                className='w-80 my-5 transition-transform duration-300 group-hover:scale-105'
-                                src={`${imageBaseUrl}${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                            
-                            <p className='absolute top-5 right-0 bg-red-600 text-white text-md p-1 border rounded-sm'>
-                                Rating: {movie.vote_average}
-                            </p>
+                {popularMovies.length === 0 && searchValue.trim() !== '' ? (
+                    <div>No movies found.</div>
+                ) : (
+                    popularMovies.map((movie) => (
+                        <div className='flex my-10 flex-col justify-center items-center w-[500px] group' key={movie.id}>
+                            <h1 className='w-80 my-4 flex justify-center'>{movie.title}</h1>
+                            <div className='relative'>
+                                <img
+                                    className='w-80 my-5 transition-transform duration-300 group-hover:scale-105'
+                                    src={`${imageBaseUrl}${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                
+                                <p className='absolute top-5 right-0 bg-red-600 text-white text-md p-1 border rounded-sm'>
+                                    Rating: {movie.vote_average.toFixed(1)}
+                                </p>
+                            </div>
+                            <div className='bg-orange-600 px-5 p-1 border rounded-sm hover:bg-orange-500 my-1'>
+                                {isAuthenticated ? (
+                                    <Link className='text-white' onClick={() => handleViewDetail(movie)}>
+                                        View Detail
+                                    </Link>
+                                ) : (
+                                    <span className='text-gray-400'>View Detail</span>
+                                )}
+                            </div>
+    
                         </div>
-                        <div className='bg-orange-600 px-5 p-1 border rounded-sm hover:bg-orange-500 my-1'>
-                            {isAuthenticated ? (
-                                <Link className='text-white' onClick={() => handleViewDetail(movie)}>
-                                    View Detail
-                                </Link>
-                            ) : (
-                                <span className='text-gray-400'>View Detail</span>
-                            )}
-                        </div>
-
-                    </div>
-                ))}
+                    ))
+                )}
             </div>
         </div>
     );
+    
 }
 
 export default APIPage;
